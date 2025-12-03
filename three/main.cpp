0c9ceab59bd2b7b2f45d6350f013f1ee8f525112
varshil247 <varshil247@gmail.com>
2025-12-03 23:37:32 +0000

    Day three, part two complete. Efficient monotonic stack method.

--- three/main.cpp
+++ three/main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <climits>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
+/*
 int main ()
 {
   int total_joltage = 0;
@@ -35,5 +37,50 @@ int main ()
   }
   
   cout << "Total Joltage: " << total_joltage << endl;
+  return 0;
+}
+*/
+
+
+// 234234234234278
+// 2
+// 2 3 -> 3 is bigger, pop 2, you can now only remove 2 # remove case
+// 3
+// 3 4 -> 4 is bigger, pop 3, you can now only remove 1
+// 4
+// 4 2 -> 2 is smaller
+// 4 2 3 -> 3 is bigger, pop 2, you can now only remove 0
+// 4 3 .... ans # keep case
+
+int main ()
+{
+  long long total_joltage = 0;
+
+  ifstream file("test_final.txt");
+  string str_line;
+  
+  while (getline(file, str_line)) {
+    int str_line_len = str_line.length();  
+    int removable = str_line_len - 12;
+    vector<char> joltage;
+
+    for (char curr_char: str_line) {
+      while (!joltage.empty() && removable > 0 && joltage.back() < curr_char) {
+        joltage.pop_back();
+        removable--;
+      }
+      joltage.push_back(curr_char); 
+    }
+
+    while (!joltage.empty() && removable > 0) {
+      joltage.pop_back();
+      removable--;
+    }
+    
+    string max_joltage_string(joltage.begin(), joltage.end());
+    cout << max_joltage_string << endl;
+    total_joltage += stoll(max_joltage_string);
+  } 
+  cout << endl <<"Total Joltage: " << total_joltage << endl;
   return 0;
 }
